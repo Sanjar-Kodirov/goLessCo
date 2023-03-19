74d6af9d624835f1b55420c0b9c85a33b77079d8
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-03-19 09:01:34 -0700

    webpack config decomposition

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,42 +1,21 @@
-import  HtmlWebpackPlugin  from 'html-webpack-plugin'
-import path from 'path';
 import webpack from 'webpack';
-import { ProgressPlugin } from 'webpack';
+import path from "path";
+import { BuildPaths } from './config/build/types/config';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 
-
-
-const config: webpack.Configuration =  {
-    // production mode or development
-    mode: 'development',
+const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: "[name].[contenthash].js", // content has for making new hashed element
-        path: path.resolve(__dirname, 'build'),
-        clean: true
-    },
-    module: {
-        // rules extention for files  like png, jpg, tsx, ts, etc.
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            // for using index.html as template
-            template: path.resolve(__dirname, 'public', 'index.html')
-        }),
-        new ProgressPlugin()
-    ],
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+}
 
-};
+const mode = 'development';
+const isDev = mode === 'development';
 
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths,
+    isDev,
+})
 
-
-export default config
+export default config;
