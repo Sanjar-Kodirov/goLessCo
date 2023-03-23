81634ff250620c853deba9ac3bcf74064109d40a
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-03-22 20:52:43 -0700

    color palettes are selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { Suspense } from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import { Link, Route, Routes } from 'react-router-dom';
+import './styles/index.scss';
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
-        <div className="app">
+        <div className="app light ">
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
