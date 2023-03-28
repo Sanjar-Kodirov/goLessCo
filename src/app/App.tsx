d9564fa345ddd05c39a3f4ae7302dde4d890b701
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-03-28 00:14:41 -0700

    navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,13 @@ import { classNames } from '../shared/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/routes';
 import { Link } from 'react-router-dom';
+import { Navbar } from 'widgets/Navbar';
 const App = () => {
     const { theme, toggleTheme } = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>CLick me</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <button className="text-3xl font-bold underline" onClick={toggleTheme}>CLick me</button>
+            <Navbar />
             <AppRouter />
         </div>
     );
--- src/shared/classNames/classNames.ts
+++ src/shared/classNames/classNames.ts
@@ -1,22 +1,16 @@
 
 type Mods = Record<string, boolean | string>
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className]) => className)
     ]
         .join(' ');
 }
-
-
-// use function above   
-// const className = classNames('button', { 'button--disabled': true }, ['button--small', 'button--big']);
-
-// console.log(className);  // button button--disabled button--small button--big
-
 
 
 
