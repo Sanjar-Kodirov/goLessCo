a5a1e633c39247199ef75a3cf5f2a8593eed62ae
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-03-30 04:13:57 -0700

    i18- basic translate is working

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,15 +7,40 @@ import { AppRouter } from 'app/providers/routes';
 import { Link } from 'react-router-dom';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
+
+
+const Component = () => {
+    const { t, i18n } = useTranslation('');
+
+    const toggleLanguage = () => {
+        const currentLanguage = i18n.language;
+        const nextLanguage = currentLanguage === 'en' ? 'ru' : 'en';
+        i18n.changeLanguage(nextLanguage);
+    };
+
+    return (
+        <div className='component'>
+            <button onClick={toggleLanguage} >{t('Перевод')}</button>
+            <h1>{t('Тестовый пример')}</h1>
+        </div>
+    );
+};
+
 const App = () => {
     const { theme, toggleTheme } = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback={'Loading...'} >
+                <Component />
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+
         </div>
     );
 };
