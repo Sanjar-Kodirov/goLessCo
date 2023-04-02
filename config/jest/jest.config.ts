6a7cc4395938761fc528015cc0b76ec1733dae9e
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-04-02 14:15:19 +0500

    absolute path problem in jest fixed

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -21,6 +21,7 @@ export default {
     // An array of directory names to be searched recursively up from the requiring module's location
     moduleDirectories: [
         'node_modules',
+        'src',
     ],
     // A list of reporter names that Jest uses when writing coverage reports
     coverageReporters: [
