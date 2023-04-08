ddc3cf6fb6082889c01de0d8ccfc2c68f538b761
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-04-08 16:31:58 +0500

    fix imports

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-import { counterActions } from '../model/slice/CounterSlice';
+import { counterActions } from '../model/slice/counterSlice';
 import { getCounterValue } from '../selectors/getCounterValue/getCounterValue';
 
 export const Counter = () => {
